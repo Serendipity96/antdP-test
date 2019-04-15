86fb40ce4b5e42e154502a3bc955798ece08f9e4
Serendipity96 <justserendipity96@gmail.com>
2019-04-15 16:50:21 +0800

    动态显示详情页

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -6,66 +6,129 @@ const { Option } = Select;
 
 class Detail extends Component {
   state = {
-    chartData: [],
-    cpuData: [],
+    cpuChart: [],
+    memChart: [],
+    ioChart: [],
+    netChart: [],
   };
 
   componentWillMount() {
-    this.getData();
-
-    const tmp = [];
+    const cpu = [];
+    const mem = [];
+    const io = [];
+    const net = [];
     for (let i = 0; i < 20; i += 1) {
-      tmp.push({
-        x: i,
-        y2: 1.5,
+      cpu.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 1.5,
+      });
+      mem.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 1.2,
+      });
+      io.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 2.3,
+        y2: 4.5,
+      });
+      net.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 0.8,
+        y2: 3.2,
       });
     }
     this.setState({
-      chartData: tmp,
+      cpuChart: cpu,
+      memChart: mem,
+      ioChart: io,
+      netChart: net,
     });
+    this.getData();
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.getData();
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    this.timer && clearInterval(this.timer); // eslint-disable-line
   }
 
   getData() {
+    const timeEnd = Math.round(new Date().getTime() / 1000);
+    // const timeStart = timeEnd - 86400;
+    // const timeEnd = 1555316741;
+    const timeStart = 1554964655;
+    const timeGran = 1;
+    const hostId = 1;
+    const str = {
+      /* eslint-disable */
+      timeEnd: timeEnd,
+      timeStart: timeStart,
+      timeGran: timeGran,
+      hostId: hostId,
+      /* eslint-disable */
+    };
+
     fetch(`http://127.0.0.1:8081/getHostParam`, {
-      method: 'GET',
+      method: 'POST',
+      // headers: { 'Accept': 'application/json', 'Content-Type': 'application/json'},
+      body: JSON.stringify(str),
     })
       .then(res => res.text())
       .then(data => {
         const d = JSON.parse(data);
         const d1 = d.data;
         const d2 = d1['1'];
-        const d3 = d2.cpu;
-        this.setState({ cpuData: d3 });
+
+        const cpu = [];
+        const mem = [];
+        const io = [];
+        const net = [];
+        for (let i = 0; i < d2.cpu.length; i += 1) {
+          cpu.push({
+            x: new Date().getTime() + 1000 * 60 * 30 * i,
+            y1: d2.cpu[i],
+          });
+          mem.push({
+            x: new Date().getTime() + 1000 * 60 * 30 * i,
+            y1: d2.memory[i],
+          });
+          io.push({
+            x: new Date().getTime() + 1000 * 60 * 30 * i,
+            y1: d2.ioRead[i],
+            y2: d2.ioWrite[i],
+          });
+          net.push({
+            x: new Date().getTime() + 1000 * 60 * 30 * i,
+            y1: d2.netSend[i],
+            y2: d2.netReceive[i],
+          });
+        }
+        this.setState({
+          cpuChart: cpu,
+          memChart: mem,
+          ioChart: io,
+          netChart: net,
+        });
       });
   }
 
-  handleChange(value) {
-    const { cpuData } = this.state;
-    const tmp = [];
-    if (value === '0') {
-      for (let i = 0; i < cpuData.length; i += 1) {
-        tmp.push({
-          x: new Date().getTime() + 1000 * 60 * 30 * i,
-          y1: cpuData[i],
-        });
-      }
-      this.setState({ chartData: tmp });
-    }
-  }
-
   render() {
-    const { chartData } = this.state;
+    const { cpuChart, memChart, ioChart, netChart } = this.state;
+
     return (
       <div>
-        <Select
-          placeholder="请选择机器"
-          style={{ width: '30%' }}
-          // eslint-disable-next-line
-          onChange={this.handleChange.bind(this)}
-        >
+        <Select defaultValue="0" placeholder="请选择机器" style={{ width: '30%' }}>
           <Option value="0">机器一</Option>
         </Select>
-        <TimelineChart height={300} data={chartData} titleMap={{ y1: 'cpu使用率' }} />
+
+        <TimelineChart height={300} data={cpuChart} titleMap={{ y1: 'cpu使用率' }} />
+        <TimelineChart height={300} data={memChart} titleMap={{ y1: '内存使用率' }} />
+        <TimelineChart height={300} data={ioChart} titleMap={{ y1: 'io读', y2: 'io写' }} />
+        <TimelineChart height={300} data={netChart} titleMap={{ y1: '网络上传', y2: '网络下载' }} />
       </div>
     );
   }

2c65a41a3d3d990157342af465b139cecb8c594f
SERENDIPITY96 <justserendipity96@gmail.com>
2019-06-02 23:13:54 +0800

    完善监控图表页面

--- src/pages/Detail/Chart.js
+++ src/pages/Detail/Chart.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Select, Row, Col } from 'antd';
+import { Select, Row, Col, DatePicker, Button } from 'antd';
 import { TimelineChart, WaterWave } from '@/components/Charts';
+import moment from 'moment';
 
 const { Option } = Select;
 const getDetailUrl = 'http://127.0.0.1:8081/getHostParam';
+const getMachineListUrl = 'http://127.0.0.1:8081/getMachineList';
 
 class Chart extends Component {
   state = {
@@ -13,6 +15,13 @@ class Chart extends Component {
     netChart: [],
     connectionsChart: [],
     tpsChart: [],
+    keyBufferReadChart: [],
+    keyBufferWriteChart: [],
+    tableLocksChart: [],
+    threadCacheHitChart: [],
+    machineList: [{ id: '0', ip_address: '0.0.0.0' }],
+    id: 1,
+    timestamp: 1559465597,
   };
 
   componentWillMount() {
@@ -22,6 +31,10 @@ class Chart extends Component {
     const net = [];
     const connections = [];
     const tps = [];
+    const keyBufferRead = [];
+    const keyBufferWrite = [];
+    const tableLocks = [];
+    const threadCacheHit = [];
     for (let i = 0; i < 20; i += 1) {
       cpu.push({
         x: new Date().getTime() + 1000 * 60 * 30 * i,
@@ -49,6 +62,22 @@ class Chart extends Component {
         x: new Date().getTime() + 1000 * 60 * 30 * i,
         y1: 0.8,
       });
+      keyBufferRead.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 0.8,
+      });
+      keyBufferWrite.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 0.8,
+      });
+      tableLocks.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 0.8,
+      });
+      threadCacheHit.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: 0.8,
+      });
     }
     this.setState({
       cpuChart: cpu,
@@ -57,32 +86,50 @@ class Chart extends Component {
       netChart: net,
       connectionsChart: connections,
       tpsChart: tps,
+      keyBufferReadChart: keyBufferRead,
+      keyBufferWriteChart: keyBufferWrite,
+      tableLocksChart: tableLocks,
+      threadCacheHitChart: threadCacheHit,
     });
-    this.getData(1);
+    this.showResult();
   }
 
   componentDidMount() {
     // this.timer = setInterval(() => {
     //   this.getData();
     // }, 1000);
+    this.getMachineList();
+    this.showResult();
   }
 
   componentWillUnmount() {
     // this.timer && clearInterval(this.timer); // eslint-disable-line
   }
 
-  getData(id) {
-    const timeEnd = Math.round(new Date().getTime() / 1000);
+  getMachineList() {
+    let _this = this;
+    fetch(getMachineListUrl, {
+      method: 'GET',
+    })
+      .then(res => res.text())
+      .then(res => {
+        _this.setState({ machineList: JSON.parse(res) });
+      });
+  }
+
+  showResult() {
+    const id = this.state.id;
+    const timeEnd = this.state.timestamp;
+    console.log(id);
+    console.log(timeEnd);
     // const timeStart = timeEnd - 86400;
     const timeStart = 1557386915;
     const timeGran = 1;
     const str = {
-      /* eslint-disable */
       timeEnd: timeEnd,
       timeStart: timeStart,
       timeGran: timeGran,
       hostId: id,
-      /* eslint-disable */
     };
 
     fetch(getDetailUrl, {
@@ -94,7 +141,7 @@ class Chart extends Component {
       .then(data => {
         const d = JSON.parse(data);
         const d1 = d.data;
-        console.log('d1');
+        console.log('fetch data');
         console.log(d1);
         const d2 = d1[id];
 
@@ -104,6 +151,11 @@ class Chart extends Component {
         const net = [];
         const connections = [];
         const tps = [];
+        const keyBufferRead = [];
+        const keyBufferWrite = [];
+        const tableLocks = [];
+        const threadCacheHit = [];
+
         for (let i = 0; i < d2.cpu.length; i += 1) {
           cpu.push({
             x: new Date().getTime() - 1000 * 60 * 30 * i,
@@ -131,6 +183,22 @@ class Chart extends Component {
             x: new Date().getTime() - 1000 * 60 * 30 * i,
             y1: d2.sqlTPS[i],
           });
+          keyBufferRead.push({
+            x: new Date().getTime() - 1000 * 60 * 30 * i,
+            y1: d2.keyBufferRead[i],
+          });
+          keyBufferWrite.push({
+            x: new Date().getTime() - 1000 * 60 * 30 * i,
+            y1: d2.keyBufferWrite[i],
+          });
+          tableLocks.push({
+            x: new Date().getTime() - 1000 * 60 * 30 * i,
+            y1: d2.tableLocks[i],
+          });
+          threadCacheHit.push({
+            x: new Date().getTime() - 1000 * 60 * 30 * i,
+            y1: d2.threadCacheHit[i],
+          });
         }
         this.setState({
           cpuChart: cpu,
@@ -139,29 +207,63 @@ class Chart extends Component {
           netChart: net,
           connectionsChart: connections,
           tpsChart: tps,
+          keyBufferReadChart: keyBufferRead,
+          keyBufferWriteChart: keyBufferWrite,
+          tableLocksChart: tableLocks,
+          threadCacheHitChart: threadCacheHit,
         });
       });
   }
 
-  handleChangeOption(id) {
-    this.getData(id);
+  chooseMachine(value) {
+    this.setState({ id: value });
+  }
+
+  changeDate(date, dateString) {
+    this.setState({ timestamp: Date.parse(dateString) });
+  }
+
+  disabledEndDate(current) {
+    return current > moment();
   }
 
   render() {
-    const { cpuChart, memChart, ioChart, netChart, connectionsChart, tpsChart } = this.state;
+    const {
+      machineList,
+      cpuChart,
+      memChart,
+      ioChart,
+      netChart,
+      connectionsChart,
+      tpsChart,
+      keyBufferReadChart,
+      keyBufferWriteChart,
+      tableLocksChart,
+      threadCacheHitChart,
+    } = this.state;
 
     return (
       <div>
         <Select
-          defaultValue="1"
           placeholder="请选择机器"
-          style={{ width: '30%', marginBottom: 15 }}
-          onChange={this.handleChangeOption.bind(this)}
+          style={{ width: '30%', marginBottom: 15, marginRight: 10 }}
+          onChange={this.chooseMachine.bind(this)}
         >
-          <Option value="1">机器一</Option>
-          <Option value="2">机器二</Option>
-          <Option value="3">机器三</Option>
+          {machineList.map(item => {
+            return (
+              <Option value={item.id} key={item.id}>
+                {item.ip_address}
+              </Option>
+            );
+          })}
         </Select>
+        <DatePicker
+          onChange={this.changeDate.bind(this)}
+          disabledDate={this.disabledEndDate.bind(this)}
+        />
+        <Button type="primary" onClick={this.showResult.bind(this)} style={{ marginLeft: 10 }}>
+          查询
+        </Button>
 
         <Row gutter={24}>
           <Col xl={12} lg={12} md={12} sm={24} xs={24}>
@@ -192,11 +294,31 @@ class Chart extends Component {
           </Col>
         </Row>
         <Row gutter={24}>
-          <Col xl={12} lg={12} md={12} sm={24} xs={24} style={{ background: '#fff' }}>
-            <WaterWave height={161} title="连接池使用" percent={28} />
+          <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+            <TimelineChart
+              height={300}
+              data={keyBufferReadChart}
+              titleMap={{ y1: ' Key Buffer读穿透率' }}
+            />
           </Col>
-          <Col xl={12} lg={12} md={12} sm={24} xs={24} style={{ background: '#fff' }}>
-            <WaterWave height={161} title="缓存表使用" percent={36} />
+          <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+            <TimelineChart
+              height={300}
+              data={keyBufferWriteChart}
+              titleMap={{ y1: ' Key Buffer写穿透率' }}
+            />
+          </Col>
+        </Row>
+        <Row gutter={24}>
+          <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+            <TimelineChart height={300} data={tableLocksChart} titleMap={{ y1: '锁阻塞率' }} />
+          </Col>
+          <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+            <TimelineChart
+              height={300}
+              data={threadCacheHitChart}
+              titleMap={{ y1: ' 线程缓存命中率' }}
+            />
           </Col>
         </Row>
       </div>
